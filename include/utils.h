c6bc26e89f61ab6b7f91d339a16cd52d05b43cd8
LBMFluids <truskawka.at@gmail.com>
2021-11-14 15:26:39 -0500

    Changed the 1D to 2D conversion to use vectors

--- include/utils.h
+++ include/utils.h
@@ -5,7 +5,7 @@
 
 /*************************************************************** 
  * Common utility functions 
-***************************************************************/
+ ***************************************************************/
 
 /** 
  * \brief Convert from subscript (i,j) to linear index
@@ -21,27 +21,30 @@ template<typename T>
 std::vector<T> sub2ind(std::vector<std::vector<T>>& subs, const size_t Nx);
 
 /** 
- * \brief Convert a flat array of type T to nested vector of type T
+ * \brief Convert a flat vector of type T to nested vector of type T
  * \details Assumes all inner vectors have the same size. 
- * @param Nrow [in] - number of rows (inner vectors) 
- * @param Ncol [in] - number of columns (elements in the inner vectors)
- * @param flat_arr [in] - pointer to a flat array of type T
+ * @param Nrow [in] - number of columns (elements in the inner vectors)  
+ * @param Ncol [in] - number of rows (inner vectors)
+ * @param flat_vec [in] - reference to a flat vector of type T
  * @return - vector of vectors of type T
 */
 template<typename T>
-std::vector<std::vector<T>> flat_array_2_vector_2D(const size_t Nrow, const size_t Ncol, const T* flat_arr);
+std::vector<std::vector<T>> flat_vector_2_vector_2D(const size_t Ncol, 
+								const size_t Nrow, const std::vector<T>& flat_vec);
 
 /** 
- * \brief Convert a nested vector of type T to flat array of type T
- * \details Size of the array is #inner vectors x #elements in inner vectors
+ * \brief Convert a nested vector of type T to flat vector of type T
+ * \details Size of the vector is #inner vectors x #elements in inner vectors
  * 		Number of elements in the inner vectors assumed the same across all
  * 		inner vectors and is determined based on the first vector.
- * @param vec2D - vector of vectors of type T
+ *		Final vector is: [inner_vec_1] ... [last inner vec]
+ * @param vec2D - reference to vector of vectors of type T
  * @param Nelem [in] - number of elements in flat_arr
- * @param flat_arr [out] - pointer to a flat array of type T and size Nelem
+ * @param flat_vec [in/out] - reference to a flat vector of type T and size Nelem
 */
 template<typename T>
-void vector_2D_2_flat_array(std::vector<std::vector<T>> vec2D, const size_t Nelem, T* flat_arr);
+void vector_2D_2_flat_vector(const std::vector<std::vector<T>>& vec2D, 
+								const size_t Nelem, std::vector<T>& flat_vec);
 
 /**
  * \brief Convert a string to all lower case
@@ -75,27 +78,30 @@ std::vector<T> sub2ind(std::vector<std::vector<T>>& subs, const size_t Nx)
 }
 
 template<typename T>
-std::vector<std::vector<T>> flat_array_2_vector_2D(const size_t Nrow, const size_t Ncol, const T* flat_arr)
+std::vector<std::vector<T>> flat_vector_2_vector_2D(const size_t Ncol, 
+								const size_t Nrow, const std::vector<T>& flat_vec)
 {
 	std::vector<std::vector<T>> vec2D(Nrow, std::vector<T>(Ncol));
 	size_t ind = 0;
-	for (size_t ir=0; ir < Nrow; ir++){
-		for (size_t icl = 0; icl < Ncol; icl++){
+	for (size_t ir=0; ir < Nrow; ir++) {
+		for (size_t icl = 0; icl < Ncol; icl++) {
 			ind = Ncol*ir + icl;
-			vec2D.at(ir).at(icl) = flat_arr[ind];
+			vec2D.at(ir).at(icl) = flat_vec.at(ind);
 		}
 	}
 	return vec2D;
 }
 
 template<typename T>
-void vector_2D_2_flat_array(std::vector<std::vector<T>> vec2D, const size_t Nelem, T* flat_arr)
+void vector_2D_2_flat_vector(const std::vector<std::vector<T>>& vec2D, 
+								const size_t Nelem, std::vector<T>& flat_vec)
 {
 	size_t Nrow = vec2D.size(), Ncol = vec2D.at(0).size(), ind = 0;
-	if (Nelem != Nrow*Ncol)
+	if (Nelem != Nrow*Ncol) {
 		throw std::range_error("Flat array and 2D vector sizes mismatch");
-	for (auto row : vec2D){
-		std::copy(row.begin(), row.end(), flat_arr + ind*Ncol);
+	}
+	for (auto row : vec2D) {
+		std::copy(row.begin(), row.end(), flat_vec + ind*Ncol);
 		ind++;
 	}
 }
