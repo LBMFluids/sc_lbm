2a7a9e95c207eb74ebfbd6280aee493eef5a993d
LBMFluids <truskawka.at@gmail.com>
2022-09-25 18:05:21 -0500

    Developing better regression tests in progress

--- tests/lbm/lbm_matlab/two_phase_flow/get_and_save_comparison_data.m
+++ tests/lbm/lbm_matlab/two_phase_flow/get_and_save_comparison_data.m
@@ -5,19 +5,8 @@
 clear; close all
 
 % Stationary droplet in a semi-periodic domain of bulk liquid
-mat_name = 'developed_droplet_';
+mat_name = 'Gs_0_dPdL_0_';
 comp_name = '../../test_data/matlab_stationary_droplet_';
-save_data(mat_name, comp_name);
-
-% Stationary droplet trapped by a solid boundary in a semi-periodic domain 
-% with an array of circular pillars and bulk liquid
-mat_name = 'trapped_droplet_';
-comp_name = '../../test_data/matlab_trapped_droplet_';
-save_data(mat_name, comp_name);
-
-% Droplet moving through an array of circular pillars and bulk liquid
-mat_name = 'pillars_';
-comp_name = '../../test_data/matlab_flowing_droplet_';
 save_data(mat_name, comp_name);
 
 function save_data(mat_name, comp_name)
@@ -30,6 +19,7 @@ function save_data(mat_name, comp_name)
         % Load the dataset
         fprintf('Processing: %s\n', file_names{ii})
         temp = load(file_names{ii});
+        load(file_names{ii});
         % Compute macroscopic quantities
         [rho_1, rho_2, ux_1, ux_2, uy_1, uy_2] = ...
                   macroscopic_properties(temp.f_1, temp.f_2, temp.Cur_Iter,...
@@ -40,29 +30,47 @@ function save_data(mat_name, comp_name)
         save(strcat(temp_name, 'density.txt'), "rho_1", '-ascii');
         save(strcat(temp_name, 'ux.txt'), "ux_1", '-ascii');
         save(strcat(temp_name, 'uy.txt'), "uy_1", '-ascii');
+        save(strcat(temp_name, 'ucx.txt'), "ucx_1", '-ascii');
+        save(strcat(temp_name, 'ucy.txt'), "ucy_1", '-ascii');
+        save(strcat(temp_name, 'Fx.txt'), "Fx_1", '-ascii');
+        save(strcat(temp_name, 'Fy.txt'), "Fy_1", '-ascii');
         % Then the bulk (fluid 2)
         temp_name = strcat(comp_name, 'bulk_step_', num2str(temp.Cur_Iter), '_');
         save(strcat(temp_name, 'density.txt'), "rho_2", '-ascii');
         save(strcat(temp_name, 'ux.txt'), "ux_2", '-ascii');
         save(strcat(temp_name, 'uy.txt'), "uy_2", '-ascii');
+        save(strcat(temp_name, 'ucx.txt'), "ucx_2", '-ascii');
+        save(strcat(temp_name, 'ucy.txt'), "ucy_2", '-ascii');
+        save(strcat(temp_name, 'Fx.txt'), "Fx_2", '-ascii');
+        save(strcat(temp_name, 'Fy.txt'), "Fy_2", '-ascii');
         % Save density distributions
         % Direction 9 is actual direction 0 (center, stationary) in the C++
         % code
         % First the droplet (fluid 1)
         temp_name = strcat(comp_name, 'droplet_f_step_', num2str(temp.Cur_Iter), '_');
+        temp_name_eq = strcat(comp_name, 'droplet_f_eq_step_', num2str(temp.Cur_Iter), '_');
         for ic = 1:temp.N_c-1
             temp_f = temp.f_1(:,:,ic);
             save(strcat(temp_name, num2str(ic), '.txt'), "temp_f", '-ascii');
+            temp_feq = temp.feq_1(:,:,ic);
+            save(strcat(temp_name_eq, num2str(ic), '.txt'), "temp_feq", '-ascii');
         end
         temp_f = temp.f_1(:,:,9);
         save(strcat(temp_name, '0.txt'), "temp_f", '-ascii');
+        temp_feq = temp.feq_1(:,:,9);
+        save(strcat(temp_name_eq, '0.txt'), "temp_feq", '-ascii');
         % Then the bulk (fluid 2)
         temp_name = strcat(comp_name, 'bulk_f_step_', num2str(temp.Cur_Iter), '_');
+        temp_name_eq = strcat(comp_name, 'bulk_f_eq_step_', num2str(temp.Cur_Iter), '_');
         for ic = 1:temp.N_c-1
             temp_f = temp.f_2(:,:,ic);
             save(strcat(temp_name, num2str(ic), '.txt'), "temp_f", '-ascii');
+            temp_feq = temp.feq_2(:,:,ic);
+            save(strcat(temp_name_eq, num2str(ic), '.txt'), "temp_feq", '-ascii');
         end
         temp_f = temp.f_2(:,:,9);
         save(strcat(temp_name, '0.txt'), "temp_f", '-ascii');
+        temp_feq = temp.feq_2(:,:,9);
+        save(strcat(temp_name_eq, '0.txt'), "temp_feq", '-ascii');
     end
 end
--- tests/lbm/lbm_matlab/two_phase_flow/save_geom.m
+++
@@ -1,9 +0,0 @@
-% Save geometry for the C++ part
-
-clear; close all;
-
-g_file_name = '../../test_data/droplet_tests_final_geometry.txt';
-
-temp = load('pillars_1.mat');
-geom = temp.Channel2D;
-save(g_file_name, 'geom', '-ascii');
